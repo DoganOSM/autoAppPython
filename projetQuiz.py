a88b0f803ae5399af9ce298aa14a88f339515910
Dr Doom <dogan.osolokomazola@gmail.com>
2024-09-21 23:53:53 +0200

    premier commit en py

---
+++ projetQuiz.py
@@ -0,0 +1,47 @@
+print("Bienvenue sur le quizz")
+
+player = input("Participera tu a mon quizz\n o ou n ?\n ")
+
+if player != "o":
+    quit()
+
+print(" Parfait jouons ;)")
+
+reponse = input("qui est le perso principal de dargon ball ?\n ")
+
+if reponse == "goku":
+    print("Bonne réponse (gohan devait etre le personage principal après l'arc cell normalement)")
+else:
+    print("Mauvaise Réponse revise tes classiques")
+
+reponse = input("De quel manga vien la guilde Fairy Tail ?\n ")
+
+if reponse == "Fairy Tail":
+    print("Bonne réponse ")
+else:
+    print("Mauvaise Réponse la consigne donne la reponse...")
+
+reponse = input("Quel est le manga le plus surcoté?\n ")
+
+if reponse == "SNK":
+    print("Bonne réponse ")
+else:
+    print("Mauvaise Réponse c'est mon avis...")
+
+
+reponse = input("Qui est le plus beau?\n ")
+
+if reponse == "MOI":
+    print("Bonne réponse la confiance en sois c'est sa qu'il faut!! ")
+else:
+    print("Mauvaise Réponse Ai Confience en toi !!")
+
+reponse = input("Qui est le plus fort?\n ")
+
+if reponse == "TOI":
+    print("Bonne réponse ")
+else:
+    print("Mauvaise Réponse Abuse pas de ta confience en toi mdr")
+
+
+
